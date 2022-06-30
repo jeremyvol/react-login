909c3a78c5ce1101bf817e60c0ff2fecffe005d2
Jeremy Vollant <j.vollant@gmail.com>
2022-06-30 11:02:53 +0200

    useEffect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -11,20 +11,33 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  useEffect(() => {
+    console.log('EFFECT RUNNING');
+    return () => {
+      console.log('EFFECT CLEANUP');
+    };
+  }, [enteredEmail]);
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log('Checking validity!');
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log('CLEANUP');
+      clearTimeout(identifier);
+    };
+  }, [enteredEmail, enteredPassword]);
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
   };
 
   const validateEmailHandler = () => {
@@ -46,8 +59,7 @@ const Login = (props) => {
         <div
           className={`${classes.control} ${
             emailIsValid === false ? classes.invalid : ''
-          }`}
-        >
+          }`}>
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
@@ -60,8 +72,7 @@ const Login = (props) => {
         <div
           className={`${classes.control} ${
             passwordIsValid === false ? classes.invalid : ''
-          }`}
-        >
+          }`}>
           <label htmlFor="password">Password</label>
           <input
             type="password"
